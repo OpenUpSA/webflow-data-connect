eec8c27afc44201fcc3542af87dd02f6d3f58949
Di M <meerkdir@gmail.com>
2023-06-23 12:33:24 +0200

    wip

--- webflow-data-connect.js
+++ webflow-data-connect.js
@@ -1,27 +1,50 @@
-let params = {
-limit: 250,
+$(document).ready(() => {
+
+	$('[nocodb-template]').each((i, el) => {
+
+		let table = el.attr('nocodb-table');
+		let target = el.attr('nocodb-target');
+		let $el = el.clone(true, true);
+
+		let params = {
+			limit: 250,
+		}
+
+		console.log(nocodb_query(table, params));
+
+	});
+
+})
+
+
+let nocodb_query = (table, params) => {
+
+	xios.get(nocodb + '/' + table, {
+		headers: {
+			'xc-token': xc_token
+		},
+		params: params
+	}).then(function (response) {
+		
+		return response.data;
+
+		// const item_template = $('.components [nocodb-template=]').first().clone(true, true);
+		
+		// let $el = research_item.clone(true, true);
+
+		// response.data.list.forEach((item) => {
+
+		// 	$el.find('[nocodb-field="title"]').text('hey');
+		// 	$('body').append($el);
+
+		// })
+
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+
 }
 
-axios.get(nocodb + '/' + table, {
-            headers: {
-                'xc-token': xc_token
-            },
-            params: params
-        }).then(function(response) {
-				console.log(response);
-        const research_item = $('.components .research-item__container').first().clone(true, true);
-        
-        let target = document.querySelector('body');
-  			let $el = research_item.clone(true, true);
-        
-        response.data.list.forEach((item) => {
-        	
-          $el.find('[nocodb-field="title"]').text('hey');
-					$('body').append($el);
-
-        })
 
 
-        }).catch(function(error) {
-            console.log(error);
-        });
