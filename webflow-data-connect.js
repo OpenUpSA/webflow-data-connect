c44178f88122d06d31778382e257e934c70f23d4
Di M <meerkdir@gmail.com>
2023-06-23 10:14:43 +0200

    Create webflow-data-connect.js

---
+++ webflow-data-connect.js
@@ -0,0 +1,28 @@
+let table = 'research';
+let params = {
+limit: 250,
+}
+
+axios.get(nocodb + '/' + table, {
+            headers: {
+                'xc-token': xs_token
+            },
+            params: params
+        }).then(function(response) {
+				console.log(response);
+        const research_item = $('.components .research-item__container').first().clone(true, true);
+        
+        let target = document.querySelector('body');
+  			let $el = research_item.clone(true, true);
+        
+        response.data.list.forEach((item) => {
+        	
+          $el.find('[nocodb-field="title"]').text('hey');
+					$('body').append($el);
+
+        })
+
+
+        }).catch(function(error) {
+            console.log(error);
+        });
