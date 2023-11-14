f42ea4df754e90abfec769503b5fdcbdc2dc5c54
Di M <meerkdir@gmail.com>
2023-11-14 15:12:08 +0200

    apitable work

--- webflow-data-connect.js
+++ webflow-data-connect.js
@@ -1,51 +1,7 @@
 $(document).ready(() => {
 
-	$('[nocodb-template]').each((i, el) => {
-
-		let $el = $(el);
-
-		let table = $el.attr('nocodb-table');
-		let target = $el.attr('nocodb-target');
-
-		let params = {
-			limit: 250,
-		}
-
-		console.log(nocodb_query(table, params));
-
-	});
+	console.log('hello');
 
 })
-
-
-let nocodb_query = (table, params) => {
-
-	axios.get(nocodb + '/' + table, {
-		headers: {
-			'xc-token': xc_token
-		},
-		params: params
-	}).then(function (response) {
-		
-		return response;
-
-		// const item_template = $('.components [nocodb-template=]').first().clone(true, true);
-		
-		// let $el = research_item.clone(true, true);
-
-		// response.data.list.forEach((item) => {
-
-		// 	$el.find('[nocodb-field="title"]').text('hey');
-		// 	$('body').append($el);
-
-		// })
-
-
-	}).catch(function (error) {
-		console.log(error);
-	});
-
-}
-
 
 
