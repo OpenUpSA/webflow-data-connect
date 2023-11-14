7940e64ef7b4d9201df3e0ee8d7e04182a264b21
Di M <meerkdir@gmail.com>
2023-11-14 16:16:28 +0200

    apitable

--- webflow-data-connect.js
+++ webflow-data-connect.js
@@ -8,7 +8,7 @@ function renderRecords(records) {
 		recordItem.removeClass('hidden_item');
 		recordItem.find('h2').text(record.fields.Title);
 		recordItem.find('p').text(record.fields.Description);
-		recordItem.find('.research-year').text(new Date(record.fields.Year).toLocaleString());
+		recordItem.find('.research-year').text(new Date(record.fields.Year).getFullYear());
 		// recordItem.find('.research-type').text(record.fields.Year);
 		recordItem.find('.research-access').text(record.fields.Access);
 
