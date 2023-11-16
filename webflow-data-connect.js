cb549901a5445892d5ae616ea3a86d833855a478
Di M <meerkdir@gmail.com>
2023-11-16 15:10:51 +0200

    Update webflow-data-connect.js

--- webflow-data-connect.js
+++ webflow-data-connect.js
@@ -183,10 +183,10 @@ $(document).ready(() => {
 	css.rel = 'stylesheet';
 	document.getElementsByTagName('head')[0].appendChild(css);
 
-	$('.acc_categories').append('<select multiple="multiple" class="categories_select" name="categories[]" multiple="multiple" style="width: 100%"></select>');
-	$('.acc_subjects').append('<select multiple="multiple" class="subjects_select" name="subjects[]" multiple="multiple" style="width: 100%"></select>');
-	$('.acc_types').append('<select multiple="multiple" class="types_select" name="types[]" multiple="multiple" style="width: 100%"></select>');
-	$('.acc_countries').append('<select multiple="multiple" class="countries_select" name="countries[]" multiple="multiple" style="width: 100%"></select>');
+	// $('.acc_categories').append('<select multiple="multiple" class="categories_select" name="categories[]" multiple="multiple" style="width: 100%"></select>');
+	// $('.acc_subjects').append('<select multiple="multiple" class="subjects_select" name="subjects[]" multiple="multiple" style="width: 100%"></select>');
+	// $('.acc_types').append('<select multiple="multiple" class="types_select" name="types[]" multiple="multiple" style="width: 100%"></select>');
+	// $('.acc_countries').append('<select multiple="multiple" class="countries_select" name="countries[]" multiple="multiple" style="width: 100%"></select>');
 
 	fetchRelations();
 
