8e16173090d47b39cf698e5eb68ced78d971da34
Di M <meerkdir@gmail.com>
2023-11-16 21:21:16 +0200

    Update webflow-aitable-single.js

--- webflow-aitable-single.js
+++ webflow-aitable-single.js
@@ -1,1 +1,35 @@
 
+let queryString = 'recordsIds=';
+
+fetch(aitable + recordsTable + queryString, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': 'Bearer ' + token
+		}
+	})
+	.then((response) => {
+		if (!response.ok) {
+			throw Error(response.statusText);
+		}
+		return response;
+	})
+	.then((response) => response.json())
+	.then((data) => {
+
+		console.log(data);
+
+		records = data.data.records;
+		total = data.data.total;
+		pageNum = data.data.pageNum;
+		pageSize = data.data.pageSize;
+		
+		renderRecords(records);
+		
+		$('.research-count-count').text(total);
+		
+
+	})
+	.catch((error) => {
+		console.error(error);
+	});
