e2f1eedc5e753debd504d6fc27876699c6aa9293
Di M <meerkdir@gmail.com>
2023-11-16 21:45:40 +0200

    Update webflow-aitable-single.js

--- webflow-aitable-single.js
+++ webflow-aitable-single.js
@@ -36,6 +36,46 @@ fetch(aitable + recordsTable + queryString, {
 		yearPill.text(record.fields.Year)
 		$('.research-meta').append(yearPill);
 
+		if(record.fields.Types_Lookup != undefined && record.fields.Types_Lookup.length > 0) {
+			record.fields.Types_Lookup.forEach(type => {
+				let typePill = $typePill.clone();
+				typePill.text(type);
+				recordItem.find('.research-meta').append(typePill);
+			})
+		}
+
+		let accessPill = $accessPill.clone(true, true);
+		accessPill.text(record.fields.Access)
+		$('.research-meta').append(accessPill);
+
+		if(record.fields.Subjects_Lookup != undefined && record.fields.Subjects_Lookup.length > 0) {
+			record.fields.Subjects_Lookup.forEach(sub => {
+				let subjectPill = $subjectPill.clone();
+				subjectPill.text(sub);
+				recordItem.find('.research-meta-2').append(subjectPill);
+			})
+		}
+
+		if(record.fields.Categories_Lookup != undefined && record.fields.Categories_Lookup.length > 0) {
+			record.fields.Categories_Lookup.forEach(cat => {
+				let categoryPill = $categoryPill.clone();
+				categoryPill.text(cat);
+				recordItem.find('.research-meta-2').append(categoryPill);
+			})
+		}
+
+		if(record.fields.Countries_Lookup != undefined && record.fields.Countries_Lookup.length > 0) {
+			record.fields.Countries_Lookup.forEach(country => {
+				let countryPill = $countryPill.clone();
+				countryPill.text(country);
+				recordItem.find('.research-meta-2').append(countryPill);
+			})
+		}
+
+		
+
+		
+
 		
 		
 
