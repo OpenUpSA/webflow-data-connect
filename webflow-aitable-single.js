8e49436cbc98ac3129804fe152ec6bc8f965f55e
Di M <meerkdir@gmail.com>
2023-11-16 21:42:43 +0200

    Update webflow-aitable-single.js

--- webflow-aitable-single.js
+++ webflow-aitable-single.js
@@ -20,13 +20,33 @@ fetch(aitable + recordsTable + queryString, {
 	.then((response) => response.json())
 	.then((data) => {
 
+		$typePill = $('.research-type').first().clone(true, true);
+		$subjectPill = $('.research-subject').first().clone(true, true);
+		$categoryPill = $('.research-category').first().clone(true, true);
+		$countryPill = $('.research-country').first().clone(true, true);
+		$yearPill = $('.research-year').first().clone(true, true);
+		$accessPill = $('.research-access').first().clone(true, true);
+
 		let record = data.data.records[0];
+
+		$('.research-meta').html('');
+		$('.research-meta-2').html('');
+
+		let yearPill = $typePill.clone(true, true);
+		yearPill.text(record.fields.Year)
+		$('.research-meta').append(yearPill);
+
+		
+		
 
 		$('h1').text(record.fields.Title);
 		$('.text-size-small').text(record.fields.Citation);
 		$('.rl-text-style-medium').text(record.fields.Description);
 		$('.rl-button-small').attr('href', record.fields.Link);
 		$('.research-access').text(record.fields.Access);
+		
+
+		
 		// year, type, category, subject, country
 		
 		
