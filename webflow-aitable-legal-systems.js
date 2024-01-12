2f3866771ae840364b98ca1f454345c4317bee26
Di M <meerkdir@gmail.com>
2024-01-12 15:09:57 +0200

    Update webflow-aitable-legal-systems.js

--- webflow-aitable-legal-systems.js
+++ webflow-aitable-legal-systems.js
@@ -1,1 +1,7 @@
+const url = new URL(window.location.href);
+const params = new URLSearchParams(url.search);	
+const country = params.get('country'); 
 
+$(document).ready(function() {
+	$('.country-select-wrapper').append('<select><option>Select A Country</option></select>');
+})
